eb0edece093ecf96a5d64ea0688ac415453c844f
Forty Morgan <dimazhmudikov@gmail.com>
2018-05-14 19:26:15 +0300

    Move path to variables

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -1,12 +1,20 @@
 import fs from 'fs';
 import genDiff from '../src';
 
+const expectedResult = '__tests__/__fixtures__/firstTestResult';
+
+const jsonConfig1 = '__tests__/__fixtures__/before.json';
+const jsonConfig2 = '__tests__/__fixtures__/after.json';
+
+const yamlConfig1 = '__tests__/__fixtures__/before.yml';
+const yamlConfig2 = '__tests__/__fixtures__/after.yml';
+
 test('Correct config json diff', () => {
-  expect(genDiff('__tests__/__fixtures__/before.json', '__tests__/__fixtures__/after.json'))
-    .toBe(fs.readFileSync('__tests__/__fixtures__/firstTestResult', 'utf8'));
+  expect(genDiff(jsonConfig1, jsonConfig2))
+    .toBe(fs.readFileSync(expectedResult, 'utf8'));
 });
 
 test('Correct config yaml diff', () => {
-  expect(genDiff('__tests__/__fixtures__/before.yml', '__tests__/__fixtures__/after.yml'))
-    .toBe(fs.readFileSync('__tests__/__fixtures__/firstTestResult', 'utf8'));
+  expect(genDiff(yamlConfig1, yamlConfig2))
+    .toBe(fs.readFileSync(expectedResult, 'utf8'));
 });
