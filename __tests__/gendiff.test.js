6a338963db82d8efbf7ef97db79fe295d2428938
Forty Morgan <dimazhmudikov@gmail.com>
2018-05-14 20:27:42 +0300

    Add test to ini diff

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -9,6 +9,9 @@ const jsonConfig2 = '__tests__/__fixtures__/after.json';
 const yamlConfig1 = '__tests__/__fixtures__/before.yml';
 const yamlConfig2 = '__tests__/__fixtures__/after.yml';
 
+const iniConfig1 = '__tests__/__fixtures__/before.ini';
+const iniConfig2 = '__tests__/__fixtures__/after.ini';
+
 test('Correct config json diff', () => {
   expect(genDiff(jsonConfig1, jsonConfig2))
     .toBe(fs.readFileSync(expectedResult, 'utf8'));
@@ -16,5 +19,10 @@ test('Correct config json diff', () => {
 
 test('Correct config yaml diff', () => {
   expect(genDiff(yamlConfig1, yamlConfig2))
+    .toBe(fs.readFileSync(expectedResult, 'utf8'));
+});
+
+test('Correct config ini diff', () => {
+  expect(genDiff(iniConfig1, iniConfig2))
     .toBe(fs.readFileSync(expectedResult, 'utf8'));
 });
