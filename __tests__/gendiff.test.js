e94f4ab58a5e0ef359232984e34fe41611b97543
Forty Morgan <dimazhmudikov@gmail.com>
2018-05-14 15:24:42 +0300

    Add test for yaml

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -1,7 +1,12 @@
 import fs from 'fs';
 import genDiff from '../src';
 
-test('Correct config diff', () => {
+test('Correct config json diff', () => {
   expect(genDiff('__tests__/__fixtures__/before.json', '__tests__/__fixtures__/after.json'))
+    .toBe(fs.readFileSync('__tests__/__fixtures__/firstTestResult', 'utf8'));
+});
+
+test('Correct config yaml diff', () => {
+  expect(genDiff('__tests__/__fixtures__/before.yml', '__tests__/__fixtures__/after.yml'))
     .toBe(fs.readFileSync('__tests__/__fixtures__/firstTestResult', 'utf8'));
 });
