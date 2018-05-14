2183f3946a5da1a6eb414a23f04a0a9816a552b3
Forty Morgan <dimazhmudikov@gmail.com>
2018-05-14 19:52:06 +0300

    Change form diff to unmutable algorithm

--- src/index.js
+++ src/index.js
@@ -3,28 +3,22 @@ import _ from 'lodash';
 import yaml from 'js-yaml';
 import path from 'path';
 
-const showDiff = (conf1, conf2) => {
-  const diffArray = [];
-  Object.keys(conf1).forEach((key) => {
+const formDiff = (conf1, conf2) => {
+  const bothKeys = _.union(Object.keys(conf1), Object.keys(conf2));
+  const diffArray = bothKeys.map((key) => {
     if (_.has(conf2, key)) {
-      if (conf1[key] === conf2[key]) {
-        diffArray.push(`    ${key}: ${conf2[key]}`);
-      } else {
-        diffArray.push(`  + ${key}: ${conf2[key]}`);
-        diffArray.push(`  - ${key}: ${conf1[key]}`);
+      if (_.has(conf1, key)) {
+        if (conf1[key] === conf2[key]) {
+          return `    ${key}: ${conf2[key]}`;
+        }
+        return [`  + ${key}: ${conf2[key]}`, `  - ${key}: ${conf1[key]}`];
       }
-    } else {
-      diffArray.push(`  - ${key}: ${conf1[key]}`);
+      return `  + ${key}: ${conf2[key]}`;
     }
+    return `  - ${key}: ${conf1[key]}`;
   });
 
-  Object.keys(conf2).forEach((key) => {
-    if (!_.has(conf1, key)) {
-      diffArray.push(`  + ${key}: ${conf2[key]}`);
-    }
-  });
-
-  return diffArray.join('\n');
+  return _.flatten(diffArray).join('\n');
 };
 
 const parsers = {
@@ -40,7 +34,7 @@ const genDiff = (pathToFile1, pathToFile2) => {
   const config1 = parsers[path.extname(pathToFile1)](file1);
   const config2 = parsers[path.extname(pathToFile2)](file2);
 
-  const diff = `{\n${showDiff(config1, config2)}\n}`;
+  const diff = `{\n${formDiff(config1, config2)}\n}`;
 
   return diff;
 };
