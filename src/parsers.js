fece1284b120f177870038c5d47688244c5c9ce0
Forty Morgan <dimazhmudikov@gmail.com>
2018-05-14 20:27:31 +0300

    Add ini parser

--- src/parsers.js
+++ src/parsers.js
@@ -1,10 +1,12 @@
 import yaml from 'js-yaml';
+import ini from 'ini';
 import path from 'path';
 
 const parsers = {
   '.json': JSON.parse,
   '.yml': yaml.safeLoad,
   '.yaml': yaml.safeLoad,
+  '.ini': ini.parse,
 };
 
 export default pathToFile => parsers[path.extname(pathToFile)];
