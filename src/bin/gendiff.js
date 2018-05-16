21ed0005014ab900c2a8e5e4515d6177ad584d70
Forty Morgan <dimazhmudikov@gmail.com>
2018-05-17 02:27:53 +0300

    Change package version to 1.5.5

--- src/bin/gendiff.js
+++ src/bin/gendiff.js
@@ -4,7 +4,7 @@ import program from 'commander';
 import genDiff from '..';
 
 program
-  .version('1.5.4', '-v, --version')
+  .version('1.5.5', '-v, --version')
   .description('Compares two configuration files and shows a difference.')
   .option('-f, --format [type]', 'Output format')
   .arguments('<firstConfig> <secondConfig>')
