92a2234f5c2c36bc7a564ab6a52103d2fd783c4e
Forty Morgan <dimazhmudikov@gmail.com>
2018-05-18 18:41:53 +0300

    Change package version 1.6.2

--- src/bin/gendiff.js
+++ src/bin/gendiff.js
@@ -4,7 +4,7 @@ import program from 'commander';
 import genearateDiff from '..';
 
 program
-  .version('1.6.1', '-v, --version')
+  .version('1.6.2', '-v, --version')
   .description('Compares two configuration files and shows a difference.')
   .option('-f, --format [type]', 'Output format')
   .arguments('<firstConfig> <secondConfig>')
