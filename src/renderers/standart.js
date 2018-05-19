a31fbb659f9dd7e00268f71b145ee0779819946f
Forty Morgan <dimazhmudikov@gmail.com>
2018-05-19 17:49:56 +0300

    Move space counts to constants

--- src/renderers/standart.js
+++ src/renderers/standart.js
@@ -5,21 +5,25 @@ const stringify = (value, depth) => {
     return value;
   }
   const keys = _.keys(value);
+  const valueSpaceCount = depth * 4;
+  const closeObjectSpaceCount = (depth * 4) - 4;
   const stringArray = keys
-    .map(key => `${' '.repeat(depth * 4)}${key}: ${value[key]}`);
-  return `{\n${stringArray.join('\n')}\n${' '.repeat((depth * 4) - 4)}}`;
+    .map(key => `${' '.repeat(valueSpaceCount)}${key}: ${value[key]}`);
+  return `{\n${stringArray.join('\n')}\n${' '.repeat(closeObjectSpaceCount)}}`;
 };
 
 const render = (diff, parentName = '', depth = 1) => {
   const diffToString = (diffElem) => {
     const prettifyDiffString = (value, diffSign) => {
+      const diffSpaceCount = (depth * 4) - 2;
       const valueString = stringify(value, depth + 1);
-      return `${' '.repeat((depth * 4) - 2)}${diffSign} ${diffElem.key}: ${valueString}`;
+      return `${' '.repeat(diffSpaceCount)}${diffSign} ${diffElem.key}: ${valueString}`;
     };
 
     const diffString = {
       nested: (diffNode) => {
-        const newParentName = `${' '.repeat((depth * 4) - 2)}  ${diffNode.key}: `;
+        const valueSpaceCount = depth * 4;
+        const newParentName = `${' '.repeat(valueSpaceCount)}${diffNode.key}: `;
         return render(diffNode.children, newParentName, depth + 1);
       },
       'not changed': diffNode => prettifyDiffString(diffNode.value, ' '),
@@ -37,7 +41,8 @@ const render = (diff, parentName = '', depth = 1) => {
 
   const diffStringArray = diff.map(diffElem => diffToString(diffElem));
   const diffFlatArr = _.flatten(diffStringArray);
-  return `${parentName}{\n${diffFlatArr.join('\n')}\n${' '.repeat((depth * 4) - 4)}}`;
+  const closeObjectSpaceCount = (depth * 4) - 4;
+  return `${parentName}{\n${diffFlatArr.join('\n')}\n${' '.repeat(closeObjectSpaceCount)}}`;
 };
 
 export default render;
